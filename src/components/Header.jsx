3f71181fce5dfeaa3fafcfc3c7999d9d147b7612
sivakrishna1023 <chukkakrishna999@gmail.com>
2024-05-25 16:57:10 +0530

    Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,14 @@ import {Box,AppBar,Toolbar,IconButton,Typography,Button} from "@mui/material"
 import MenuIcon from '@mui/icons-material/Menu';
 import { useDispatch, useSelector } from 'react-redux';
 import { actionCreators } from '../state/index';
+import { bindActionCreators } from 'redux';
 
 
 const Header = () => {
   const user=useSelector(state=>state.user);
   const dispatch=useDispatch();
+  const actions = bindActionCreators(actionCreators,dispatch);
+  const {LogMeIn,LogMeOut} = bindActionCreators(actionCreators,dispatch);
   // console.log(user);
   return (
         <Box sx={{ flexGrow: 1 }}>
@@ -25,8 +28,14 @@ const Header = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Redux
           </Typography>
-          {
+          {/* {
             user ?  <Button onClick={()=>{dispatch(actionCreators.LogMeIn(null))}} color="inherit">LogOut</Button> :  <Button onClick={()=>{dispatch(actionCreators.LogMeIn({name:"ramana",password:"asas"}))}}  color="inherit">LogIn</Button>
+          } */}
+          {/* {
+            user ?  <Button onClick={()=>{actions.LogMeOut(null)}} color="inherit">LogOut</Button> :  <Button onClick={()=>{actions.LogMeIn({name:"ramana",password:"asas"})}}  color="inherit">LogIn</Button>
+          } */}
+          {
+            user ?  <Button onClick={()=>{LogMeOut(null)}} color="inherit">LogOut</Button> :  <Button onClick={()=>{dispatch(LogMeIn({name:"ramana",password:"asas"}))}}  color="inherit">LogIn</Button>
           }
         </Toolbar>
       </AppBar>
